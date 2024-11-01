ee98e14a30b629e5fa4cfa3bd1a6a2c9cfd48012
UaHig <golosnyyigor@gmail.com>
2024-11-01 11:53:58 +0100

    add new colour scheme

--- src/components/Poster/Poster.jsx
+++ src/components/Poster/Poster.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 import styles from "../../styles/Home.module.css"
 
-import BG from "../../images/computer.png"
+import BG from "../../images/pngwin.png"
+
 
 const Poster = () => {
   return (
@@ -10,8 +11,8 @@ const Poster = () => {
       <div className={styles.title}>BIG SALE 80%</div>
       <div className={styles.product}>
         <div className={styles.text}>
-          <div className={styles.subtitle}>the bestseller of 2024</div>
-          <h1 className={styles.head}>LENNON r2d2 with NVIDIA 5090 TI</h1>
+          <div className={styles.subtitle}>the bestseller of Year</div>
+          <h1 className={styles.head}>LENNI r2d2 with NVIDIA 5090 AI TFXI</h1>
           <button className={styles.button}>Shop now</button>
         </div>
         <div className={styles.image}>
